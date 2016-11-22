e974ac05d78b666e8937baf7dc54f59657a554ba
Tom Esterez <testerez@gmail.com>
2016-11-21 23:29:42 -0500

    convert to typescript

--- dist/bundle.js
+++ dist/bundle.js
@@ -44,78 +44,55 @@
 /* 0 */
 /***/ function(module, exports, __webpack_require__) {
 
-	'use strict';
-
-	var _a = __webpack_require__(1);
-
-	var a = _interopRequireWildcard(_a);
-
-	function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
-
+	"use strict";
+	var a = __webpack_require__(1);
 	a.a();
+
 
 /***/ },
 /* 1 */
 /***/ function(module, exports, __webpack_require__) {
 
 	"use strict";
-
-	Object.defineProperty(exports, "__esModule", {
-	    value: true
-	});
-	exports.a2 = exports.a = undefined;
-
-	var _b = __webpack_require__(2);
-
+	var b_1 = __webpack_require__(2);
 	function a() {
-	    (0, _b.b)();
+	    b_1.b();
 	}
-
+	exports.a = a;
 	function a2() {
 	    console.log("It's ok to use circular imports");
 	}
-
+	exports.a2 = a2;
 	// Fail
 	// module.exports = { a, a2 }
-
 	// Fail
 	// would work for: import a from a;
 	// But not for: import {a} from a;
 	// export default {a, a2};
-
 	// Works
 	// Object.assign(module.exports, { a, a2 });
-
 	// Works
 	// exports.a = a;
 	// exports.a2 = a2;
-
 	// Works
-	exports.a = a;
-	exports.a2 = a2;
+
 
 /***/ },
 /* 2 */
 /***/ function(module, exports, __webpack_require__) {
 
-	'use strict';
-
-	Object.defineProperty(exports, "__esModule", {
-	    value: true
-	});
-	exports.b = b;
-
-	var _a = __webpack_require__(1);
-
-	function b() {
-	    (0, _a.a2)();
-	} // Fail
+	// Fail
 	// var a2 = require('./a').a2;
-
+	"use strict";
 	// Works. Later, call a.a2()
 	// var a = require('./a')
-
 	// Works
+	var a_1 = __webpack_require__(1);
+	function b() {
+	    a_1.a2();
+	}
+	exports.b = b;
+
 
 /***/ }
 /******/ ]);
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,18 @@
  module.exports = {
-  entry: './src/index.js',
+  entry: './src/index.ts',
   output: {
     path: './dist',
     filename: 'bundle.js'
   },
+  resolve: {
+    extensions: ['', '.ts', '.tsx', '.js', '.jsx']
+  },
   module: {
     loaders: [
-      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+      {
+        test: /\.ts$/,
+        loader: 'awesome-typescript-loader'
+      }
     ]
   }
 }
